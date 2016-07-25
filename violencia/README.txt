d863398d3e1ca2f7e9204b3b4dfd0901250018c5
Leonardo Leite <leonardofl87@gmail.com>
2016-07-25 20:56:00 -0300

    voos lookup tables

--- violencia/README.txt
+++ violencia/README.txt
@@ -1,1 +1,4 @@
 Fonte dos dados: http://www.mapadaviolencia.org.br/
+
+Dados importado são referentes a 2012
+
--- voos/consultas.sql
+++ voos/consultas.sql
@@ -1,4 +1,9 @@
 
+-- empresas aéreas em nossa base
+select distinct carrier from voos;
+select count(distinct carrier) from voos;
+select distinct c.description from voos v, carriers c where v.carrier = c.code;
+
 -- destino mais popular
 select dest_city_name, count(*) as qtd from voos group by dest_city_name order by qtd desc;
 select dest_city_name, count(*) as qtd from voos group by dest_city_name order by qtd desc limit 10;
@@ -24,8 +29,16 @@ create temp view chegadas as select dest, count(*) as qtd from voos group by des
 select p.origin, p.qtd as partidas, c.qtd as chegadas from partidas p, chegadas c where p.origin = c.dest; 
 select p.origin, p.qtd + c.qtd as movimento from partidas p, chegadas c where p.origin = c.dest order by movimento desc limit 10; 
 select p.origin, p.qtd + c.qtd as movimento, p.qtd as partidas, c.qtd from partidas p, chegadas c where p.origin = c.dest order by movimento desc limit 10; 
+select p.origin, a.description, p.qtd + c.qtd as movimento, p.qtd as partidas, c.qtd from partidas p, chegadas c, airports a where p.origin = c.dest and c.dest = a.code order by movimento desc limit 10; 
 
 -- compahias aéreas mais populares
+select distinct carrier from voos;
+select carrier, count(*) as qtd from voos group by carrier order by qtd desc;
+select c.description, count(*) as qtd from voos v, carriers c where v.carrier = c.code group by v.carrier order by qtd desc;
+
+-- rota mais popular
+select origin, dest, count(*) as qtd from voos group by origin, dest order by qtd desc limit 10;
+select a_ori.description, a_des.description, count(*) as qtd from voos v, airports a_ori, airports a_des where v.origin = a_ori.code and v.dest = a_des.code group by origin, dest order by qtd desc limit 10;
 
 -- voos mais longos
 
