8c5220342e585185afd264ff0d5ee3617f40878e
leonardofl <leonardofl87@gmail.com>
2017-08-26 09:28:58 -0300

    removendo acentos na importação do banco sp156

--- sp156/preparacao.sh
+++ sp156/preparacao.sh
@@ -7,5 +7,5 @@ echo $headers > $output
 for f in originais/*.csv
 do
     echo "Processing $f"
-    tail -n +2 $f | iconv -f ISO-8859-9 -t UTF-8 | sed -r "s_([0-9]{2})/([0-9]{2})/([0-9]{4})_\3-\2-\1_g" >> $output
+    tail -n +2 $f | iconv -f ISO-8859-9 -t UTF-8 | sed -r -e "s_([0-9]{2})/([0-9]{2})/([0-9]{4})_\3-\2-\1_g" -e "y/áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚçÇ/aAaAaAaAeEeEiIoOoOoOuUcC/" >> $output
 done
